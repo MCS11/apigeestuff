d65ee52dfe1be68778802efc84ee42cac1f28ae6
MCS11 <73001610+MCS11@users.noreply.github.com>
2020-10-19 09:05:53 +0200

    Add files via upload

--- trace.js
+++ trace.js
@@ -3,9 +3,10 @@ var sampleApp = angular.module('sampleApp', []);
 sampleApp.controller('TraceController', ['$scope', '$http','$q', function ($scope, $http) {
     $scope.traceData={};
     $scope.traceDataToShow={request:[], response:[]};
+    $scope.allPoints = {};
     $scope.selectedStep=null;
     loadTrace=function() {
-        $http.get("/json/sampleTraceInvalid.json").then(function(response){
+        $http.get("/json/trace-1602921486393.json").then(function(response){
             //console.log(response);
             $scope.traceData = response.data;
             //console.log($scope.traceData);
@@ -38,6 +39,12 @@ sampleApp.controller('TraceController', ['$scope', '$http','$q', function ($scop
 //            console.log(this);
 //        });
         angular.forEach(traceData.auditData.Point, function(step, key) {
+            if (!$scope.allPoints[step.id]) {
+                //console.log("add to list", step.id);
+                $scope.allPoints[step.id]={};
+                $scope.allPoints[step.id].visible=true;
+            }
+            //console.log($scope.allPoints);
             if (step.DebugInfo!=null) {
                 //console.log(step, key);
                 if (step.ResponseMessage!=null) {
@@ -61,6 +68,37 @@ sampleApp.controller('TraceController', ['$scope', '$http','$q', function ($scop
         }
         return "";
     }
+    $(document).keydown(function(e){
+        if(e.which==39) {
+            console.log("right");
+            $(".teBorderColorSelected").each(function(element){
+                console.log(this);
+                console.log("parent",angular.element(this).parent(function(
+                ){
+                    console.log("parent", this.getAttribute("class"));
+                }));
+                $(".teBorderColorSelected").next(".traceElement").each(function(){
+                    console.log(this);
+                    angular.element(this).triggerHandler('click');
+                });
+            });
+        }
+        if(e.which==37) {
+            console.log("left");
+            $(".teBorderColorSelected").each(function(element){
+                console.log(this);
+                $(".teBorderColorSelected").prev(".traceElement").each(function(){
+                    console.log(this);
+//                    $scope.selectItem({target:this},null);
+                    angular.element(this).triggerHandler('click');
+                });
+            });
+        }
+    });
+    $scope.traceKeyPressed = function($event,$this,step) {
+        console.log("traceKeyPressed");
+        console.log("$event,$this,step",$event,$this,step);
+    };
     $scope.getTextFromStep=function(step) {
         if (step.id=="Condition") {
 //            console.log(step.DebugInfo.Properties.Property)
@@ -70,11 +108,14 @@ sampleApp.controller('TraceController', ['$scope', '$http','$q', function ($scop
         return "";
     };
     $scope.showItem = function(step) {
-        if (step.id=="FlowInfo") {
-            return false;
-        }
-        if (step.id=="DebugMask") {
-            return false;
+//        if (step.id=="FlowInfo") {
+//            return false;
+//        }
+//        if (step.id=="DebugMask") {
+//            return false;
+//        }
+        if ($scope.allPoints[step.id]) {
+            return $scope.allPoints[step.id].visible;
         }
         return true;
     };
@@ -94,6 +135,11 @@ sampleApp.controller('TraceController', ['$scope', '$http','$q', function ($scop
                 retClasses.push("teStateChange");
                 break;
             };
+            case "FlowCallout":
+            case "FlowReturn" : {
+                retClasses.push("teFlowSmall");
+                break;
+            }
             case "FlowInfo": {
                 if(step.DebugInfo.Properties && step.DebugInfo.Properties.Property) {
                     retClasses.push("teFlowInfo");
@@ -109,7 +155,7 @@ sampleApp.controller('TraceController', ['$scope', '$http','$q', function ($scop
                 if(step.DebugInfo.Properties && step.DebugInfo.Properties.Property) {
                     //console.log("stepType21",step.DebugInfo.Properties.Property);
                     var stepType = getProperty(step.DebugInfo.Properties.Property,"type");   
-                    console.log("stepType21", stepType, step.DebugInfo.Properties);
+                    //console.log("stepType21", stepType, step.DebugInfo.Properties);
 //                    console.log("stepType2", step.DebugInfo.Properties.Property);
                     if(stepType && stepType!="") {
                         retClasses.push("te_"+stepType);
@@ -135,9 +181,14 @@ sampleApp.controller('TraceController', ['$scope', '$http','$q', function ($scop
     };
     $scope.selectItem = function(element,step) {
         $scope.selectedStep = step;
-        console.log(element.target);
-        var stepType = getProperty(step.DebugInfo.Properties.Property,"stepDefinition-type");  
-        console.log("stepType", stepType);
+        //console.log(element.target);
+        var stepType = "";
+        if (step!=null) {
+            if (step.DebugInfo.Properties && step.DebugInfo.Properties.Property) {
+                stepType = getProperty(step.DebugInfo.Properties.Property,"stepDefinition-type");  
+            }
+        }
+        //console.log("stepType", stepType);
         if ($(element.target).hasClass('traceElement')) {
             $(".traceElement").removeClass('teBorderColorSelected');
             $(element.target).addClass('teBorderColorSelected');
